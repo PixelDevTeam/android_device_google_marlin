c238e2ad7fb6405ada0fa1bbdaac9ef678999edb
Lunarixus <Lunarixus@raphielgang.org>
2020-02-13 16:14:06 +0000

    marlin: Disable vbmeta
    Signed-off-by: Lunarixus <Lunarixus@raphielgang.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -9,3 +9,7 @@ TARGET_KERNEL_SOURCE := kernel/google/marlin
 TARGET_KERNEL_CONFIG := marlin_defconfig
 TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+
+# Disable vbmeta
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
